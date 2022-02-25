a1dd9383daf816a4016275a44c1a1911f946d348
K-Vardhan <kritivardhan020@gmail.com>
2022-02-25 23:46:29 +0530

    Create one more annotations

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -9,6 +9,16 @@ import WebViewer from '@pdftron/webviewer'
 export default {
     name: 'WebViewer',
     props: { initialDoc: { type: String }},
+
+    data() {
+    return {
+      annotations: [
+        { page: 2, x: 150, y: 150, w: 50, h: 20, id: "1" },
+        { page: 1, x: 80, y: 90, w: 50, h: 20, id: "2" },
+        { page: 1, x: 120, y: 120, w: 50, h: 20, id: "3" },
+      ],
+    };
+  },
     
     mounted(){
         WebViewer(
